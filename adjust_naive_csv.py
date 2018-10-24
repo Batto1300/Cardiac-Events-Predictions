4680274810584724cec5a72da671b98f338d0588
Batto1300 <tommasoricci96@gmail.com>
2018-10-24 18:13:38 +0100

    Change 0 to Yes and 1 to No (I know.. I know..)

--- adjust_naive_csv.py
+++ adjust_naive_csv.py
@@ -1,10 +1,11 @@
 import csv
 import re
-import os
 
 
 ORIGINAL_FILE = 'datasets/CardiacDataOriginal.csv'
 TEMP_FILE = 'datasets/CardiacDataAdj.csv'
+YES = 'Yes'
+NO = 'No'
 
 reader = csv.reader(open(ORIGINAL_FILE,'r'))
 writer = csv.writer(open(TEMP_FILE,'w'))
@@ -14,7 +15,13 @@ row = next(reader)
 while row:
     new_row = []
     for value in row:
-        new_row.append(re.sub(',', '.', value))
+        if value == '0':
+            new_value = YES
+        elif value == '1':
+            new_value = NO
+        else:
+            new_value = re.sub(',', '.', value)
+        new_row.append(new_value)
     writer.writerow(new_row)
     try:
         row = next(reader)
